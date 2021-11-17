4d2592b31956b3d2d91bd64074e6c98917e773d1
kitsune2905 <fuchs.stefan2905@gmail.com>
2021-11-17 17:56:15 +0100

    update with csv

--- workclock.sh
+++ workclock.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 x=0
-w=$(date)
-echo -e '\e[38;5;216mZeitaufzeichnung gestartet um '$w
+w=$(date '+%Y-%m-%d')
+e=$(date '+%H:%M:%S')
+echo -e '\e[38;5;216mZeitaufzeichnung gestartet am '$w' um '$e
 echo -e 'zum Beenden die Taste \e[38;5;255ma\e[38;5;216m drücken\e[38;5;255m'
 
 while true
@@ -17,9 +18,10 @@ y=$(($x%3600))
 z=$(($x/3600))
 q=$((y/60))
 v=$(date)
+f=$(date '+%H:%M:%S')
 echo ''
 echo -e '\e[38;5;6m---------------------------------------------------'
-echo 'begonnen um '$w' beendet um '$v
+echo 'begonnen am '$w' um '$e' beendet um '$f
 echo -e '\e[38;5;12m'$z' Stunden, '$q' Minuten gearbeitet\e[38;5;255m'
 echo ''
 echo -e '\e[38;5;216mzum Speichern Taste \e[38;5;255ms\e[38;5;216m drücken, zum Abbrechen drücke Taste \e[38;5;255mr'
@@ -31,6 +33,7 @@ while true
 			echo -e '\e[38;5;6m---------------------------------------------------' >> zeitspeicher
 			echo 'begonnen um '$w' beendet um '$v >> zeitspeicher
 			echo -e '\e[38;5;12m'$z' Stunden, '$q' Minuten gearbeitet\e[38;5;255m' >> zeitspeicher
+			echo $w' , '$e' ,'$f' , '$z':'$q >> zeittable.csv
 			break
 		fi
 		if [[ $l = r ]]; then
@@ -39,3 +42,4 @@ while true
 		fi
 	done
 cat zeitspeicher
+cat zeittable.csv
